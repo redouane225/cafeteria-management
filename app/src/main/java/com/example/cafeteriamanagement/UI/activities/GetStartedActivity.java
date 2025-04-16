edb373eec06f2a3b8d7e45ced6648f6d2c01c1e6
redouane <abdelbarisofianeredouane@gmail.com>
2025-04-16 14:49:44 +0000

    system before changing it to dummy data

--- app/src/main/java/com/example/cafeteriamanagement/UI/activities/GetStartedActivity.java
+++ app/src/main/java/com/example/cafeteriamanagement/UI/activities/GetStartedActivity.java
@@ -2,24 +2,15 @@ package com.example.cafeteriamanagement.UI.activities;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.cafeteriamanagement.R;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
-
-import com.example.cafeteriamanagement.R;
 
 public class getStarted_Activity extends AppCompatActivity {
 
